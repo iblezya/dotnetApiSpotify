8e433f311ed13c4508926f9e0c37a35b300f26a9
Gerson Jean Pierre <iblezya@gmail.com>
2021-11-23 17:28:19 -0500

    Add HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,34 +4,65 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SpotifyApi.Models;
+using SpotifyApi.Services;
 
 namespace SpotifyApi.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ISpotifyAccountService _spotifyAccountService;
+    private readonly IConfiguration _configuration;
+    private readonly ISpotifyService _spotifyService;
+
+    public HomeController(
+        ISpotifyAccountService spotifyAccountService,
+        IConfiguration configuration,
+        ISpotifyService spotifyService)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _spotifyAccountService = spotifyAccountService;
+      _configuration = configuration;
+      _spotifyService = spotifyService;
     }
+
+    public async Task<IActionResult> Index()
+    {
+      var newReleases = await GetReleases();
+
+      return View(newReleases);
+    }
+
+    private async Task<IEnumerable<Release>> GetReleases()
+    {
+      try
+      {
+        var token = await _spotifyAccountService.GetToken(
+          _configuration["Spotify:ClientId"],
+          _configuration["Spotify:ClientSecret"]);
+
+        var newReleases = await _spotifyService.GetNewReleases("PE", 20, token);
+
+        return newReleases;
+      }
+      catch (Exception ex)
+      {
+        Debug.Write(ex);
+
+        return Enumerable.Empty<Release>();
+      }
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
